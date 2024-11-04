63d466c6e2922444c6b8e36fe1fa2ec57714abda
Alessandro Poletti <pole97@live.it>
2024-11-04 16:18:33 +0100

    Controller + DAO tests

---
+++ code/server/src/test_unit/controller_tests/document.test.ts
@@ -0,0 +1,36 @@
+import { test, expect, jest, describe } from "@jest/globals";
+import DocumentController from "../../controllers/documentController";
+import DocumentDAO from "../../dao/documentDAO";
+import { InvalidLinkError } from "../../errors/documentError";
+
+describe("Document Controller", () => {
+
+    let documentController: DocumentController;
+
+    beforeEach(() => {
+        documentController = new DocumentController();
+        jest.mock("../../dao/documentDAO");
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe("listDocuments", () => {
+
+        test("should resolve true", async () => {
+            jest.spyOn(DocumentDAO.prototype, "linkDocuments").mockResolvedValueOnce(true);
+            const result = await documentController.linkDocuments(1, 2, "direct consequence");
+
+            expect(DocumentDAO.prototype.linkDocuments).toHaveBeenCalledTimes(1);
+            expect(result).toBe(true);
+        });
+
+        test("should throw InvalidLinkError", async () => {
+            jest.spyOn(DocumentDAO.prototype, "linkDocuments").mockResolvedValueOnce(true);
+            await expect(documentController.linkDocuments(1, 1, "direct consequence")).rejects.toThrow(InvalidLinkError);
+        });
+        
+    });
+
+});
---
+++ code/server/src/test_unit/dao_tests/document.test.ts
@@ -0,0 +1,66 @@
+import { describe, test, expect, jest } from "@jest/globals";
+import DocumentDAO from "../../dao/documentDAO";
+import db from "../../db/db";
+import { Database } from "sqlite3";
+import { DuplicateLinkError } from "../../errors/documentError";
+
+describe("Document DAO", () => {
+
+    let documentDAO: DocumentDAO;
+
+    beforeEach(() => {
+        documentDAO = new DocumentDAO();
+        jest.mock("../../db/db");
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe("linkDocuments", () => {
+
+        test("should resolve true", async () => {
+            const mockDBRun = jest
+                .spyOn(db, "run")
+                .mockImplementation((sql, [], callback) => {
+                    callback(null);
+                    return {} as Database;
+                });
+            
+            const result = await documentDAO.linkDocuments(1, 2, "direct consequence");
+
+            expect(db.run).toHaveBeenCalledTimes(1);
+            expect(db.run).toHaveBeenCalledWith(
+                "INSERT INTO DocumentConnections VALUES (?,?,?)",
+                [1, 2, "direct consequence"],
+                expect.any(Function)
+              );
+            expect(result).toBe(true);
+
+            mockDBRun.mockRestore();
+        });
+
+        test("should throw DuplicateLinkError", async () => {
+            const duplicateError = { errno: 19 };
+            const mockDBRun = jest
+                .spyOn(db, "run")
+                .mockImplementation((sql, [], callback) => {
+                    callback(duplicateError);
+                    return {} as Database;
+                });
+            
+            await expect(documentDAO.linkDocuments(1, 2, "direct consequence")).rejects.toThrow(DuplicateLinkError);
+
+            expect(db.run).toHaveBeenCalledTimes(1);
+            expect(db.run).toHaveBeenCalledWith(
+                "INSERT INTO DocumentConnections VALUES (?,?,?)",
+                [1, 2, "direct consequence"],
+                expect.any(Function)
+              );
+
+            mockDBRun.mockRestore();
+        });
+
+    });
+
+});
