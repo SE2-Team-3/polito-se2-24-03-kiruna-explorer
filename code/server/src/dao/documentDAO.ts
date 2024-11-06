18cc23d9276a2f7da92d4925fd43e6a5f8016b49
SaraPoiatti <s309616@studenti.polito.it>
2024-11-06 01:06:39 +0100

    update documentDao

--- code/server/src/dao/documentDAO.ts
+++ code/server/src/dao/documentDAO.ts
@@ -17,8 +17,10 @@ class DocumentDAO {
   ): Promise<any> {
     return new Promise<any>((resolve, reject) => {
       try {
-        const documentIdSql = "SELECT MAX(documentId) AS documentId FROM Document";
-        const georeferenceIdSql = "SELECT MAX(georeferenceId) AS georeferenceId FROM Georeference";
+        const documentIdSql =
+          "SELECT MAX(documentId) AS documentId FROM Document";
+        const georeferenceIdSql =
+          "SELECT MAX(georeferenceId) AS georeferenceId FROM Georeference";
         const createDocumentSql =
           "INSERT INTO Document (documentId, title, description, documentType, scale, nodeType, stakeholders, issuanceDate, language, pages, georeferenceId) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         const createGeoreferenceSql =
@@ -34,7 +36,8 @@ class DocumentDAO {
           if (row.documentId) documentId = row.documentId + 1;
           db.get(georeferenceIdSql, [], (err: Error | null, row: any) => {
             if (err) return reject(err);
-            if (row.georeferenceId) georeferenceId = georeference ? row.georeferenceId + 1 : null;
+            if (row.georeferenceId)
+              georeferenceId = georeference ? row.georeferenceId + 1 : null;
             db.run(
               createDocumentSql,
               [
@@ -82,7 +85,11 @@ class DocumentDAO {
     });
   }
 
-  linkDocuments(documentId1: number, documentId2: number, linkType: string): Promise<boolean> {
+  linkDocuments(
+    documentId1: number,
+    documentId2: number,
+    linkType: string
+  ): Promise<boolean> {
     return new Promise<boolean>((resolve, reject) => {
       try {
         const sql = "INSERT INTO DocumentConnections VALUES (?,?,?)";
@@ -98,93 +105,95 @@ class DocumentDAO {
     });
   }
 
-    linkDocuments(documentId1:number,documentId2:number,linkType:string): Promise<boolean> {
-        return new Promise<boolean>((resolve,reject)=>{
-            try {
-                const sql='INSERT INTO DocumentConnections VALUES (?,?,?)'
-                db.run(sql,[documentId1,documentId2,linkType],(err:any)=>{
-                    if (err) {
-                        if (err.errno===19) reject(new DuplicateLinkError)
-                        else reject(err)
-                    }
-                    else resolve(true)
-                })
-            } catch (error) {
-                reject(error)
+  georeferenceDocument(
+    documentId: number,
+    georeference: string[]
+  ): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      try {
+        const georeferenceIdSql =
+          "SELECT MAX(georeferenceId) AS georeferenceId FROM Georeference";
+        const updateDocumentSql =
+          "UPDATE Document SET georeferenceId=? WHERE documentId=?";
+        const createGeoreferenceSql = "INSERT INTO Georeference VALUES (?, ?)";
+        db.get(georeferenceIdSql, (err: Error | null, row: any) => {
+          if (err) return reject(err);
+          const georeferenceId = row.georeferenceId
+            ? row.georeferenceId + 1
+            : 1;
+          db.run(
+            createGeoreferenceSql,
+            [georeferenceId, JSON.stringify(georeference)],
+            (err: Error | null) => {
+              if (err) return reject(err);
+              db.run(
+                updateDocumentSql,
+                [georeferenceId, documentId],
+                (err: Error | null) => {
+                  if (err) reject(err);
+                  else resolve(true);
+                }
+              );
             }
-        })
-    }
+          );
+        });
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
-    georeferenceDocument(documentId:number,georeference:string[]): Promise<boolean> {
-        return new Promise<boolean>((resolve,reject)=>{
-            try {
-                const georeferenceIdSql = "SELECT MAX(georeferenceId) AS georeferenceId FROM Georeference";
-                const updateDocumentSql =
-                    "UPDATE Document SET georeferenceId=? WHERE documentId=?";
-                const createGeoreferenceSql = "INSERT INTO Georeference VALUES (?, ?)";
-                db.get(georeferenceIdSql,(err:Error|null,row:any)=>{
-                    if (err) return reject(err)
-                    const georeferenceId=row.georeferenceId?row.georeferenceId+1:1
-                    db.run(createGeoreferenceSql,[georeferenceId,JSON.stringify(georeference)],(err:Error|null)=>{
-                        if(err) return reject(err)
-                        db.run(updateDocumentSql,[georeferenceId,documentId],(err:Error|null)=>{
-                            if(err) reject(err)
-                            else resolve(true)
-                        })
-                    })
-                })
-            } catch (error) {
-                reject(error)
-            }
-        })
-    }
+  /**
+   * Solution for duplicates in Georeference table
+   * Sort coordinates list for areas before inserting in table
+   * Check for already existing georeference in table with findGeoreference (sort coordinate list before search)
+   * If match is found reuse the returned georeferenceId
+   * If match is not found insert new georeference
+   *
+   * Possibly add counter to Georeference table to keep track of number of documents using each georeference
+   * If one record reaches 0 documents can be deleted
+   */
+  findGeoreference(georeference: string): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      try {
+        const georeferenceIdSql =
+          "SELECT georeferenceId FROM Georeference WHERE coordinates=?";
+        db.get(
+          georeferenceIdSql,
+          [georeference],
+          (err: Error | null, row: any) => {
+            if (err) return reject(err);
+            resolve(row.georeferenceId);
+          }
+        );
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
-    /**
-     * Solution for duplicates in Georeference table
-     * Sort coordinates list for areas before inserting in table
-     * Check for already existing georeference in table with findGeoreference (sort coordinate list before search)
-     * If match is found reuse the returned georeferenceId
-     * If match is not found insert new georeference
-     * 
-     * Possibly add counter to Georeference table to keep track of number of documents using each georeference
-     * If one record reaches 0 documents can be deleted
-     */
-    findGeoreference(georeference:string): Promise<boolean> {
-        return new Promise<boolean>((resolve,reject)=>{
-            try {
-                const georeferenceIdSql = "SELECT georeferenceId FROM Georeference WHERE coordinates=?";
-                db.get(georeferenceIdSql,[georeference],(err:Error|null,row:any)=>{
-                    if (err) return reject(err)
-                    resolve(row.georeferenceId)
-                })
-            } catch (error) {
-                reject(error)
-            }
-        })
+  /**
+   * Algorithm for clockwise check
+   * It may not work in some situations, it's not mathematically proven
+   */
+  clockwiseCheck(coordinates: string): boolean {
+    let count = 0,
+      i = 0;
+    let angles: number[];
+    const coordinateArray: number[][] = JSON.parse(coordinates);
+    const newOrigin = coordinateArray[0];
+    for (const coord of coordinateArray.slice(1, -1)) {
+      let newCoord: number[] = [];
+      newCoord[0] = coord[0] - newOrigin[0];
+      newCoord[1] = coord[1] - newOrigin[1];
+      angles[i++] = Math.atan2(newCoord[0], newCoord[1]); //This fails when crossing 0° need to find a fix
     }
-
-    /**
-     * Algorithm for clockwise check
-     * It may not work in some situations, it's not mathematically proven
-     */
-    clockwiseCheck(coordinates:string): boolean {
-        let count=0, i=0
-        let angles:number[]
-        const coordinateArray:number[][]=JSON.parse(coordinates)
-        const newOrigin=coordinateArray[0]
-        for (const coord of coordinateArray.slice(1,-1)) {
-            let newCoord:number[]=[]
-            newCoord[0]=coord[0]-newOrigin[0]
-            newCoord[1]=coord[1]-newOrigin[1]
-            angles[i++]=Math.atan2(newCoord[0],newCoord[1]) //This fails when crossing 0° need to find a fix
-        }
-        for (i=1;i<angles.length;i++) {
-            if (angles[i]<angles[i-1]) count++
-            else count--
-        }
-        return count>0
+    for (i = 1; i < angles.length; i++) {
+      if (angles[i] < angles[i - 1]) count++;
+      else count--;
     }
-
+    return count > 0;
+  }
 }
 
 export default DocumentDAO;
