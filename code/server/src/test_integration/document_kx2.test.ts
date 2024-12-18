e05bb1945e5b24f36d4f73c430f3193045740ab9
Alessandro Poletti <pole97@live.it>
2024-12-18 15:26:52 +0100

    second duplicate code fix

--- code/server/src/test_integration/document_kx2.test.ts
+++ code/server/src/test_integration/document_kx2.test.ts
@@ -3,7 +3,7 @@ import request from "supertest";
 import { app } from "../../index";
 import { cleanup } from "../../src/db/cleanup";
 import db from "../../src/db/db";
-import { routePath } from "./testUtility";
+import { postUser, postDocument, postTypes, login, routePath } from "./testUtility";
 
 const planner = {
     username: "planner",
@@ -44,52 +44,6 @@ const testDocument2 = {
 };
 
 let plannerCookie: string;
-
-const postUser = async (userInfo: any) => {
-    await request(app).post(`${routePath}/users`).send(userInfo).expect(200);
-};
-
-const postDocument = async (documentInfo: any, cookie: string) => {
-    await request(app)
-        .post(`${routePath}/documents`)
-        .send(documentInfo)
-        .set("Cookie", plannerCookie)
-        .expect(201);
-};
-
-const postTypes = async () => {
-    const insertDocumentTypes = `INSERT INTO DocumentType (documentTypeName) VALUES ("Text")`;
-    const insertNodeTypes = `INSERT INTO NodeType (nodeTypeName) VALUES ("Design document")`;
-    const insertStakeholders = `INSERT INTO Stakeholder (stakeholderName) VALUES ("LKAB")`;
-    const insertStakeholders2 = `INSERT INTO Stakeholder (stakeholderName) VALUES ("Municipality")`;
-    db.run(insertDocumentTypes, [], (err) => {
-        if (err) console.log(err);
-        db.run(insertNodeTypes, [], (err) => {
-            if (err) console.log(err);
-            db.run(insertStakeholders, [], (err) => {
-                if (err) console.log(err);
-                db.run(insertStakeholders2, [], (err) => {
-                    if (err) console.log(err);
-                });
-            });
-        });
-    });
-};
-
-const login = async (userInfo: any) => {
-    return new Promise<string>((resolve, reject) => {
-        request(app)
-            .post(`${routePath}/sessions`)
-            .send(userInfo)
-            .expect(200)
-            .end((err, res) => {
-                if (err) {
-                    reject(err);
-                }
-                resolve(res.header["set-cookie"][0]);
-            });
-    });
-};
 
 describe("Document Route kx2 integration tests", () => {
 
