8457c161f2dbad07225a9523c7a4c063a8464a75
Alessandro Poletti <pole97@live.it>
2024-12-18 15:26:52 +0100

    second duplicate code fix

--- code/server/src/test_integration/testUtility.ts
+++ code/server/src/test_integration/testUtility.ts
@@ -1,7 +1,5 @@
-import { describe, test, expect, beforeAll, afterAll } from "@jest/globals";
 import request from "supertest";
 import { app } from "../../index";
-import { cleanup } from "../../src/db/cleanup";
 import db from "../../src/db/db";
 
 const routePath = "/api";
